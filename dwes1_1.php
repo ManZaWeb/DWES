4d9043fbdf11a28c6567910e00c1b0b3676e8886
Mario <maalnu93@hotmail.com>
2023-10-17 12:23:21 +0200

    Hello World

---
+++ dwes1_1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World!</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Hello world";
+        ?>
+    </h1>
+        <p>
+            <?php
+                print "Pinta bien";
+            ?>
+        </p>
+    
+</body>
+</html>
